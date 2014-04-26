ca1c6dc9f51856b5cf5f90474fbf6429263911e1
vamshireddy <k.vamshi2008@gmail.com>
2014-04-26 11:50:11 +0000

    Added ls,cd,exit,clear,mkdir,rmdir

--- README.md
+++ README.md
@@ -3,20 +3,5 @@ Vshell
 
 This is a very interactive shell with minimal list of features when compared to bash. Done exclusively for learning purpose.
 
-Tasks to be done :
-
-PARSER
-1) Implement parser for parsing the input commands
-    ( Should handle routines for | , >
-2) Structure the input into command_name, argumentlist
-3) Should handle string args ( ex: grep )\n
-
-MAIN
-1) Get the user name and the machine name for displaying at the command prompt
-2) Initialize the path variable
-
-PROCESS HANDLING
-1) Should fork a new process and replace its image with exec, when a command is entered.
-Its output is returned back using pipes.
-2) Duplicate the descriptors using dup,dup2
-3) stdin(0), stdout(1), stderr(2)
+Update 26-04-14:
+Implemented cd, ls commands with clear screen facilty. It also shows how many seconds the user has spent on the shell.
--- definetions.h
+++ definetions.h
@@ -2,17 +2,53 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <stdlib.h>
-
 #define MAX__SIZE 1024
 #define TRUE 1
 #define FALSE 0
-
 #define BACKGROUND_PROC 1
 #define IO_REDIRECT 2
 #define PIPING 3
+#define SUCCESS 1
+#define FAIL -1
+#define COMMAND_SIZE 20
+#define NORMAL_OPERATION 4
+#define BASIC_COMMAND 1
+#define MAX_SIZE 1024
+#define ARG_SIZE 10
 
-void display_prompt();
-void clear_screen();
-void welcome_prompt();
-void parse_and_find_operation();
-void execute_program();
+char privilage_char = '$';
+
+time_t start_time;
+time_t end_time;
+
+void start_timer()
+{
+	start_time = time(NULL);
+}
+
+void end_timer()
+{
+	end_time = time(NULL);
+}
+
+void handle_duration_details()
+{
+	int diff = end_time-start_time;
+	printf("You have spent %d seconds in Vshell\n",diff);
+}
+void display_prompt()
+{
+	char command_prompt[MAX_SIZE];
+	char host_name[MAX_SIZE];
+	gethostname(host_name,MAX_SIZE);
+	getcwd(command_prompt,MAX_SIZE);
+	printf("%s @ %s%c ",host_name,command_prompt,privilage_char);
+}
+void welcome_prompt()
+{
+	printf("\t\t\tWelcome to VShell ( Vamshi Shell )\n");
+}
+void clear_screen()
+{
+	system("clear");
+}
